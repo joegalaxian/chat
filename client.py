601423bcc8b605c186e67e1af0c2c1770bdfce4c
joegalaxian <joegalaxian@users.noreply.github.com>
2017-01-27 13:36:32 +1100

    Add files via upload

--- client.py
+++ client.py
@@ -1,14 +1,21 @@
 # http://www.bogotobogo.com/python/python_network_programming_server_client.php
 
 # echo_client.py
-import color
-import os
-import sys
 import socket
+import os
+import color
+import sys
 import select
 
-KNOWN_HOSTS = [('atuin.optus.com.au',4747), ('192.168.1.7',4747), ('192.168.1.11',4747), ('192.168.0.23',4747), ('192.168.0.26',4747), ('127.0.0.1',4747), ('localhost',4747)]
+KNOWN_HOSTS	= [('atuin.optus.com.au',4747), ('10.123.0.94',4747)]
+LOCAL_HOST	= [('127.0.0.1',4747), ('localhost',4747)]
+EXIT_CODE	= '/quitting'
 
+# Network IP constructor (192.168.1.1/192.168.255.255).
+def network():
+	for i in range(256):
+		for j in range(256):
+			yield '192.168.%d.%d' % (i,j)
 
 """"
 # host = socket.gethostname()
@@ -31,36 +38,39 @@ def welcome():
 	tittle = 'C H A T  -  C L I E N T'
 	print tittle
 
+EXIT_CODE = '/quitting'
+# Terminates program after closing socket and optional printing.
+def goodbye(sock = None, msg = None):
+	if sock:
+		sock.send(EXIT_CODE)
+		sock.close()
+	if msg: print msg
+	quit()
 
 # Enters host and port
 def enter_host_port():
 	host = raw_input("[*] Enter host (default '127.0.0.1'): ")
 	if host == '':
 		host = '127.0.0.1'
-
 	port = raw_input("[*] Enter port (default 4747): ")
 	if port == '':
 		port = 4747
 	else:
 		port = int(port)
-
 	return host, port
-
 
 # Connects to the server
 def connect2(host, port):
 	print "[*] Connecting to...", (host, port)
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((host, port))
-	print "[!]", color.OKGREEN + "Connected to...", (host, port), color.ENDC
+	print "[!]", color.OKGREEN + "Connected", (host, port), color.ENDC
 	return s
-
 
 # manual_connect
 def manual_connect():
 	host, port = enter_host_port()
 	return connect2(host, port)
-
 
 # auto_connect
 def auto_connect(host=None, port=None):
@@ -79,58 +89,61 @@ def auto_connect(host=None, port=None):
 			# print "[>] DEBUG Error:", str(e)
 			exception_stack.append("[!] " + color.FAIL + str(e) + " " + str(h) + color.ENDC)
 			pass
-	
 	# Print exception stack and error.
 	for e in exception_stack: print e
 	print "[!]", color.FAIL + "Couldn't connect to any known host.", color.ENDC
-
 	# manual_connect():
 	return manual_connect()
-
 #else: return connect(host, port)
 
+# Creates socket, connect, and listen 5.
+def start():
+	try:
+		print "[*] Starting..."
+		s = connect(KNOWN_HOSTS + list(network()) + LOCAL_HOST)
+		return s
+	except Exception as e:
+		goodbye(None, "[!]" + color.FAIL + " Error: " + str(e) + color.ENDC)
 
-EXIT_CODE = '/quitting'
-# Terminates program after closing socket and optional printing.
-def goodbye(sock = None, msg = None):
-	if sock:
-		sock.send(EXIT_CODE)
-		sock.close()
-	if msg: print msg
-	quit()
-
+# Creates, connects, listen and return socket if successfull, or None if unsuccessfull.
+def connect(host_list):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	for h in host_list:
+		try:
+			# print "[>] DEBUG Connecting to...", h
+			s.connect((h,4747))
+			print "[!]", color.OKGREEN + "Started at " + str(h) + color.ENDC
+			s.listen(5)
+			print "[*] Listening... (max 5)"
+			return s
+		except Exception as e:
+			print "[>] DEBUG " + str(e)
+			pass
+	raise Exception("Couldn't connect.")
 
 # loop
 def loop(s):
 	SOCKET_LIST = [s, sys.stdin]
 	name = raw_input("[*] Name: ")
-	
 	#prompt()
 	sys.stdout.write(name + "> ")
 	sys.stdout.flush()
-
 	while True:
-
 		#socket_select()
 		ready_to_read, ready_to_write, in_error = select.select(SOCKET_LIST, [], [])
-
 		#ready_to_read()
 		for sock in ready_to_read:
-
-			# server_message()
 			if sock == s:
-				# ()
+				# server_message()
 				# Message from the server -> receive, print
 				msg = s.recv(1024)
-				
 				#server_down()
 				if not msg:
 					goodbye(s, "\n[!] "+color.FAIL+"Disconnected from server."+color.ENDC)
-
 				#print_message()
 				sys.stdout.write('\r'+msg+'\n')
 				sys.stdout.flush()
-
 			# client_message()
 			elif sock == sys.stdin:
 				# User entered a chat -> send to server
@@ -141,7 +154,7 @@ def loop(s):
 					continue
 				elif msg == 'q':
 					#disconnect and terminate
-					goodbye(s, 'Quiting...')
+					goodbye(s, "[!] Program terminated.")
 				else:
 					#send_message()
 					msg = name + ": " + msg
@@ -150,7 +163,7 @@ def loop(s):
 		""" TODO: IMPROVE
 		#in_error()
 		for sock in in_error:
-			
+
 			#server fail:
 			if sock == s:
 				print "[!]", color.FAIL+"Disconnected from server."+color.ENDC
@@ -162,17 +175,13 @@ def loop(s):
 		sys.stdout.write(name + "> ")
 		sys.stdout.flush()
 
-
-
-
-
 # Program
 def main():
 	welcome()
 	s = auto_connect()
+	#s = start()
 	loop(s)
 	goodbye(s, "[*] Program terminated.")
-
 
 if __name__ == "__main__":
 	main()
--- server.py
+++ server.py
@@ -1,29 +1,19 @@
 # http://www.bogotobogo.com/python/python_network_programming_server_client.php
 
-# echo_server.py
 import socket
-import color
 import os
+import color
 import select
 
-"""
-host = ''	# Symbolic name meaning all available interfaces
-port = 12345
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host, port))
-print "[*] Listening on...", (host,port)
-s.listen(1)
-conn, addr = s.accept()
-print '[!] Connected', addr
-while True:
-	data = conn.recv(1024)
-	if not data: break
-	conn.sendall(data)
-conn.close()
-"""
+KNOWN_HOSTS	= [('atuin.optus.com.au',4747), ('10.123.0.94',4747)]
+LOCAL_HOST	= [('127.0.0.1',4747), ('localhost',4747)]
+EXIT_CODE	= '/quitting'
 
-KNOWN_HOSTS = [('atuin.optus.com.au',4747), ('10.123.0.94',4747), ('192.168.1.7',4747), ('192.168.1.11',4747), ('192.168.0.23',4747), ('192.168.0.26',4747), ('127.0.0.1',4747), ('localhost',4747)]
-
+# Network IP constructor (192.168.1.1/192.168.255.255).
+def network():
+	for i in range(256):
+		for j in range(256):
+			yield '192.168.%d.%d' % (i,j)
 
 # Prints program tittle:
 def welcome():
@@ -31,6 +21,80 @@ def welcome():
 	tittle = 'C H A T  -  S E R V E R'
 	print tittle
 
+# Terminates program after closing socket and optional printing.
+def goodbye(sock = None, msg = None):
+	if sock:
+		sock.send(EXIT_CODE)
+		sock.close()
+	if msg: print msg
+	quit()
+
+# Creates socket, connect, and listen 5.
+def start():
+	try:
+		print "[*] Starting..."
+		s = connect(KNOWN_HOSTS + list(network()) + LOCAL_HOST)
+		return s
+	except Exception as e:
+		goodbye(None, "[!]" + color.FAIL + " Error: " + str(e) + color.ENDC)
+
+# Creates, connects, listen and return socket if successfull, or None if unsuccessfull.
+def connect(host_list):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	for h in host_list:
+		try:
+			# print "[>] DEBUG Connecting to...", h
+			s.bind(h)
+			print "[!]", color.OKGREEN + "Started at " + str(h) + color.ENDC
+			s.listen(5)
+			print "[*] Listening... (max 5)"
+			return s
+		except Exception as e:
+			# print "[>] DEBUG " + str(e)
+			pass
+	raise Exception("Couldn't connect.")
+
+# Main loop
+def loop(s):
+	hosts = []
+	SOCKET_LIST = [s]
+	while True:
+		#socket_select()
+		ready_to_read, ready_to_write, in_error = select.select(SOCKET_LIST, [], [])
+		# ready_to_read()
+		for sock in ready_to_read:
+			# new_connection()
+			if sock == s:
+				sockfd, addr = s.accept()
+				SOCKET_LIST.append(sockfd)
+				print "[+]", color.WARNING + "Connection", addr, color.ENDC
+				#print '[>] DEBUG SOCKET_LIST', SOCKET_LIST
+			else:
+				#new_message()
+				# Client message -> receive and broadcast (to all except server socket and sender socket).
+				msg = sock.recv(1024)
+				if not msg:
+					# client_down()
+					print "[-]", color.WARNING+"Disconnection.", sock.getpeername(), color.ENDC
+					SOCKET_LIST.remove(sock)
+					continue
+				elif msg != '':
+					# disconnection()
+					if msg == EXIT_CODE:
+						print "[-]", color.WARNING+"Disconnection.", sock.getpeername(), color.ENDC
+						SOCKET_LIST.remove(sock)
+						continue
+				# broadcast()
+				else:
+					for host in SOCKET_LIST:
+						if host != sock and host != s:
+							#print "[>] DEBUG Sending '" +msg+ "' to ", host
+							host.send(msg)
+		# in_error()
+		for sock in in_error:
+			SOCKET_LIST.remove(sock)
+			print "[!]", color.FAIL + "Socket in error.", sock, color.ENDC
 
 """
 # Enter host and port
@@ -38,15 +102,12 @@ def enter_host_port():
 	host = raw_input("[*] Enter host (default '127.0.0.1'): ")
 	if host == '':
 		host = '127.0.0.1'
-
 	port = raw_input("[*] Enter port (default 4747): ")
 	if port == '':
 		port = 4747
 	else:
 		port = int(port)
-
 	return host, port
-
 
 # Binds socket
 def get_socket(host, port):
@@ -55,12 +116,11 @@ def get_socket(host, port):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.bind((host, port))
 		print "[!] Socket bound."
-		return s	
+		return s
 	except Exception as e:
 		print "[!] Error:", str(e)
 		s.close()
 		quit()
-
 
 # Listening
 def listening(s):
@@ -73,79 +133,14 @@ def listening(s):
 	print "[!] Listening (max " + str(i) + ")..."
 """
 
-EXIT_CODE = '/quitting'
-# Main loop
-def loop(s):
-	hosts = []
-	SOCKET_LIST = [s]
-
-	while True:
-
-		#socket_select()
-		ready_to_read, ready_to_write, in_error = select.select(SOCKET_LIST, [], [])
-		
-		# ready_to_read()
-		for sock in ready_to_read:
-			
-			# new_connection()
-			if sock == s:
-				sockfd, addr = s.accept()
-				SOCKET_LIST.append(sockfd)
-				print "[+]", color.WARNING + "Connection", addr, color.ENDC
-				#print '[>] DEBUG SOCKET_LIST', SOCKET_LIST
-			
-			#new_message()
-			else:
-				# Client message -> receive and broadcast (to all except server socket and sender socket).
-				msg = sock.recv(1024)
-				if msg != '':
-					
-					# disconnection()
-					if msg == EXIT_CODE:
-						print "[-]", color.WARNING+"Disconnection."+color.ENDC
-						SOCKET_LIST.remove(sock)
-						continue
-					
-					# broadcast()
-					else:
-						for host in SOCKET_LIST:
-							if host != sock and host != s:
-								#print "[>] DEBUG Sending '" +msg+ "' to ", host
-								host.send(msg)
-
-		# in_error()
-		for sock in in_error:
-			SOCKET_LIST.remove(sock)
-			print "[!]", color.FAIL + "Socket in error.", sock, color.ENDC
-			
-
-# Creates socket, connects to known hosts, and listen 5.
-def start():
-	print "[*] Starting..."
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	for h in KNOWN_HOSTS:
-		try:
-			s.bind(h)
-			print "[!]", color.OKGREEN + "Started at " + str(h) + color.ENDC
-			s.listen(5)
-			print "[*] Listening... (max 5)"
-			return s
-		except Exception as e:
-			# print "[!]", color.FAIL + str(e), str(h) + color.ENDC
-			pass
-
-
 # Main program
 def main():
 	welcome()
 	# host, port = enter_host_port()
 	# s = get_socket(host,port)
 	s = start()
-	# listening(s)
 	loop(s)
-	s.close()
-
+	goodbye(s)
 
 if __name__ == "__main__":
 	main()
